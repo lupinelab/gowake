881fc43de1fec0d1fa1f455a1ad10d613da27a5f
jedrw <jed@lupinelab.co.uk>
2022-10-15 04:53:14 +0100

    v1.0.0

--- cmd/gowake.go
+++ cmd/gowake.go
@@ -23,18 +23,22 @@ var gowakeCmd = &cobra.Command{
 	Short: "Send a magic packet in order wake a compliant machine",
 	Args:  cobra.ExactArgs(1),
 	Run: func(cmd *cobra.Command, args []string) {
+		// Get port
 		port, _ := cmd.Flags().GetInt("port")
 
+		// Check for mac address
 		if len(args) < 1 {
 			fmt.Println("Please provide a MAC address")
 			return
 		}
 
+		// Build packet
 		mp, err := gowake.BuildMagicPacket(args[0])
 		if err != nil {
 			return
 		}
 
+		// Send packet
 		gowake.SendPacket(mp, port)
 		fmt.Printf("Sent magic packet to %v\n", args[0])
 	},
